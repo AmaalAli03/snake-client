8cd2b5e93274bfa12c5de760e34e1ae911b8f235
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-18 19:37:45 -0500

    made changes in play.js file to make it look more neat

--- play.js
+++ play.js
@@ -1,24 +1,19 @@
 
 
 // establishes a connection with the game server
-const { connect } = require("./client.js");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+console.log("Connecting ...");
+connect();
+setupInput();
 
-
-const setupInput = function (conn) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // \u0003 maps to ctrl+c input
-    process.exit();
-  }
-  if (key === 'w') { // \u0003 maps to ctrl+c input
-    process.exit();
-  }
-};
-const conn = connect();
-setupInput(conn)
+// const handleUserInput = function (key) {
+//   if (key === '\u0003') { // \u0003 maps to ctrl+c input
+//     process.exit();
+//   }
+//   // if (key === 'w') { // \u0003 maps to ctrl+c input
+//   //   process.exit();
+//   // }
+// };
+// const conn = connect();
+// setupInput(conn)
